1d657f7402bba0adb172f459119ef5f75eb4f9ea
lucaseveron <veronlucasezequiel@hotmail.es>
2025-05-22 16:14:49 -0300

    oka

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,21 @@
-variable "learned-maker-459200-v6" {}
-variable "us-central1"     {}
-variable "service_name" {
-  default = "hola-mundo"
+variable "project_id" {
+  description = "learned-maker-459200-v6"
+  type        = string
 }
+
+variable "region" {
+  description = "us-central1"
+  type        = string
+}
+
+variable "service_name" {
+  description = "hola-mundo"
+  type        = string
+  default     = "hola-mundo"
+}
+
+variable "image_url" {
+  description = "URL de la imagen en Artifact Registry o Container Registry"
+  type        = string
+}
+
