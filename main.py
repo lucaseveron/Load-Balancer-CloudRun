8cb3423e400580ca86191761469ffc888c3793d7
lucaseveron <veronlucasezequiel@hotmail.es>
2025-05-22 23:52:20 -0300

    pou

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def hello():
     return "Hola mundo desde Cloud Run!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run usa esta variable
     app.run(host="0.0.0.0", port=port)
+
 
 
 
